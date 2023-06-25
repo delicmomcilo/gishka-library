4aefcbb02b45d0bf59ce12f34a105463d3f41c4d
Momcilo Delic <momcilodelic@gmail.com>
2023-06-25 18:30:37 +0200

    Various starter components for the library

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+export { Button, buttonVariants, type ButtonProps } from './components/Button';
+export { Avatar, AvatarFallback, AvatarImage } from './components/Avatar';
+export {Alert, AlertDescription, AlertTitle } from './components/Alert';
+export { Tabs, TabsList, TabsTrigger, TabsContent } from './components/Tabs';
+export {
+    navigationMenuTriggerStyle,
+    NavigationMenu,
+    NavigationMenuList,
+    NavigationMenuItem,
+    NavigationMenuContent,
+    NavigationMenuTrigger,
+    NavigationMenuLink, 
+    NavigationMenuIndicator,
+    NavigationMenuViewport,
+  } from './components/NavigationMenu';
+
+export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent } from './components/Card';
+
+export { Badge, badgeVariants } from './components/Badge';
